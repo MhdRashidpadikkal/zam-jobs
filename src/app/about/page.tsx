07344fc06979796fe8cf32c55837cbc9b3d72d7d
MhdRashidpadikkal <mhdrashid142@gmail.com>
2025-06-20 15:36:46 +0530

    feat: working process component added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,12 +7,14 @@ import BlogSection from "@/app/about/components/BlogSection";
 import BestWorkingWith from "@/app/about/components/BestWorkingWith";
 import TopFirms from "@/app/about/components/TopFirms";
 import AboutFirst from "@/app/about/components/AboutFirst";
+import WorkingProcess from "./components/WorkingProcess";
  
  
 export default function AboutPage() {
   return (
     <Container maxWidth="lg" sx={{ py: 3 }}>
       <AboutFirst />
+      <WorkingProcess />
      <AccordionMessage />
      <TopFirms />
       <BestWorkingWith />
