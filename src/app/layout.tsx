b6beefd63b4b7eb155e45de5292db2443cb05838
sadique ameen <sadiqeameenk@gmail.com>
2025-06-21 14:50:46 +0530

    refactor:modified jobpage mobile toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,18 @@
-
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Providers from './providers';
 import { CssBaseline } from "@mui/material";
+import type { Metadata } from "next";
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
 });
+
+export const metadata: Metadata = {
+  title: "Zam Job",
+  description: "Job portal and management system",
+};
 
 export default async function RootLayout({
   children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
+"use client";
+
 import { Container, Typography } from '@mui/material';
 import Hero from '@/components/Hero';
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Zam Job",
-  description: "Job portal and management system",
-};
 
 export default function Home() {
   return (
